a32b7f1c4913529ea08032c5de47bd2c9d0b74ff
Marvin Zavala <hazielzs@hotmail.com>
2024-05-23 01:44:12 -0600

    Last deploy Vercel

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 
 export const getTaskRequest = async () => {
-  return await axios.get("http://localhost:4000/task");
+  return await axios.get("https://task-app-react-gules.vercel.app/task");
 };
 
 export const createTaskRequest = async (task) => {
-  return await axios.post("http://localhost:4000/task", task);
+  return await axios.post("https://task-app-react-gules.vercel.app/task", task);
 };
 
 export const deleteTaskRequest = async (id) => {
-  return await axios.delete(`http://localhost:4000/task/${id}`);
+  return await axios.delete(
+    `https://task-app-react-gules.vercel.app/task/${id}`
+  );
 };
 
 export const getTaskRequestById = async (id) => {
-  return await axios.get(`http://localhost:4000/task/${id}`);
+  return await axios.get(`https://task-app-react-gules.vercel.app/task/${id}`);
 };
 
 export const updateTask = async (task, id) => {
-  return await axios.put(`http://localhost:4000/task/${id}`, task);
+  return await axios.put(
+    `https://task-app-react-gules.vercel.app/task/${id}`,
+    task
+  );
 };
